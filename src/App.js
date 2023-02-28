60ecce525e8084dd00bac20ec193538744f91778
Kishoiyan Charity <kishoiyancharity@mosess-MacBook-Air.local>
2023-02-28 16:23:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React from 'react';
+import { FaStar } from "react-icons/fa";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = {
+  orange: "#FFBA5A",
+  grey: "white"
 }
+
+function App() {
+
+  const stars= Array(5).fill(0);
+  const [currentValue, setCurrentValue] =React.useState(0);
+  const [hoverValue, setHoverValue] =React.useState(undefined);
+
+
+  const handleClick= value => {
+    setCurrentValue(value)
+  }
+
+  const handleMouseOver = value => {
+    setHoverValue(value)
+  }
+
+  const handleMouseLeave = () => {
+    setHoverValue(undefined)
+  }
+
+
+
+  return (
+
+    <div>
+      
+
+      <h1>Rating Stars</h1>
+      
+      <div className='star'>
+        
+
+        {stars.map((_, index) =>{
+          return(
+            <FaStar
+            key={index}
+            size={24}
+            style={{
+              
+            }}
+            color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
+            onClick={() => handleClick(index + 1) }
+            onMouseOver={() => handleMouseOver(index + 1)}
+            onMouseLeave={handleMouseLeave}
+            />
+          )
+        })}
+      </div>
+
+    <div className='text'>
+      <textarea
+      placeholder='Your feedback'
+      />
+
+    </div>
+
+      <button>Submit</button>
+
+    </div>
+    
+
+  );
+};
+
+
+// const styles = {
+//   container: {
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "centre",
+//   }
+// }
+
 
 export default App;
